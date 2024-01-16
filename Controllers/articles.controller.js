3848c8f3955c615a86a061fc694bd92fbb5b54a6
Jack Smith <mr.smithjsk@gmail.com>
2024-01-16 12:29:59 +0000

    Task 5 done

--- Controllers/articles.controller.js
+++ Controllers/articles.controller.js
@@ -1,15 +1,33 @@
-const { selectArticleById } = require('../Models/articles.model.js');
+const { selectArticleById, selectAllArticles } = require('../Models/articles.model.js');
 
 exports.getArticleById = async (request, response, next) => {
     const { article_id } = request.params;
+
     try {
         const article = await selectArticleById(article_id);
-        if(!article) {
-            return response.status(404).json({ msg: 'Article not found'});
-        }
         response.status(200).json({ article });
     } catch (error) {
-        console.error('Error getting article by ID:', error);
-        next(error);
+        if (error.status === 404) {
+            response.status(404).json({ msg: 'Article not found' });
+        } else {
+            console.error('Error getting article by ID:', error);
+            next(error);
+        }
     }
-}
+};
+
+exports.getAllArticles = async (request, response, next) => {
+    const { sort_by } = request.query;
+
+    try {
+        const articles = await selectAllArticles(sort_by);
+        response.status(200).json({ articles });
+    } catch (error) {
+        if (error.status === 404) {
+            response.status(404).json({ msg: "Article not found" });
+        } else {
+            console.error('Error getting all articles:', error);
+            next(error);
+        }
+    }
+};
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getAllTopics, getAllEndPoints } = require('./Controllers/topics.controller.js');
-const { getArticleById } = require('./Controllers/articles.controller.js');
+const { getArticleById, getAllArticles } = require('./Controllers/articles.controller.js');
 const app = express();
 
 app.get('/api/topics', getAllTopics);
 app.get('/api', getAllEndPoints);
 app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles', getAllArticles)
 
 module.exports = app;
