b9d6a5029c652e20b4fa5108594f3b8435cd3f1f
Jack Smith <mr.smithjsk@gmail.com>
2024-01-16 12:14:30 +0000

    Task 5 done

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
+//const jest-sorted = require('jest-sorted');
 
 afterAll(() => {
     return db.end();
@@ -15,7 +16,6 @@ beforeEach(() => {
 describe("/api/topics", () => {
     it("200: GET /api/topics should return an array of topics", async () => {
         const response = await request(app).get('/api/topics');
-        console.log(response.body);
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('topics');
         expect(response.body.topics).toBeInstanceOf(Array);
@@ -42,11 +42,59 @@ describe("GET /api/articles/:article_id", () => {
         const response = await request(app).get('/api/articles/1');
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('article')
+        const { article } = response.body;
+        expect(article).toHaveProperty('author');
+        expect(article).toHaveProperty('title');
+        expect(article).toHaveProperty('article_id');
+        expect(article).toHaveProperty('body');
+        expect(article).toHaveProperty('topic');
+        expect(article).toHaveProperty('created_at');
+        expect(article).toHaveProperty('votes');
+        expect(article).toHaveProperty('article_img_url');
     })
     it("should return 404 if invalid article ID", async () => {
         const response = await request(app).get('/api/articles/9999');
         expect(response.status).toBe(404);
-        expect(response.body.msg).toBe('Article not found')
+        expect(response.body).toHaveProperty('msg', 'Article not found');
     })
+})
+
+describe("GET /api/articles", () => {
+    it("should return all articles, with no body property, in ascending order sorted by date", async () => {
+        const response = await request(app).get('/api/articles');
+        expect(response.status).toBe(200);
+        expect(response.body.articles.length).toBe(13);
+        //expect(response.body).toBeSortedBy("created_at", {descending: true});
+        const articles = response.body.articles;
+        const sortedArticles = articles.slice().sort((a, b) => {
+            return new Date(b.created_at) - new Date(a.created_at);
+          });
+        expect(articles).toEqual(sortedArticles);
+        articles.forEach((article) => {
+            expect(article).not.toHaveProperty('body');
+          });
+    });
+    it("each reply must respond with an object which includes author, title, article_id, etc", async () => {
+        const response = await request(app).get("/api/articles");
+        expect(response.status).toBe(200);
+        expect(response.body.articles.length).toBe(13);
+        const articles = response.body.articles;
+        articles.forEach((article) => {
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("string");
+        });
+    });
+    it('should return 404 if no articles found', async () => {
+        const response = await request(app).get('/api/articles?sort_by=nonexistenttopic');
+        expect(response.status).toBe(404);
+        expect(response.body).toMatchObject({ msg: 'Article not found' });
+      });
+
 })
 
