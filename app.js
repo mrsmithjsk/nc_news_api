c9a28903d86523bef47845a5d35cc372927a9359
Jack Smith <mr.smithjsk@gmail.com>
2024-02-05 10:57:19 +0000

    allow cors

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const { getAllTopics, getAllEndPoints } = require('./Controllers/topics.controll
 const { getArticleById, getAllArticles, patchArticleById } = require('./Controllers/articles.controller.js');
 const { getAllCommentsFromID, postCommentById, deleteCommentById } = require('./Controllers/comments.controller.js');
 const { getAllUsers } = require('./Controllers/users.controller.js');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/api/topics', getAllTopics);
 app.get('/api', getAllEndPoints);
